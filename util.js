61644e9a99395e6d23072070b89c35d2642d2190
lipunmishra <53727376+lipunmishra@users.noreply.github.com>
2019-08-05 23:43:42 +0530

    Add files via upload

---
+++ util.js
@@ -0,0 +1,12 @@
+function validateUser() {
+  var username=document.getElementById('username').value;
+  var password=document.getElementById('password').value;
+  if ("admin"==username && "admin"==password) {
+    alert('hey ur sucessfully looged in ,accept the alert');
+    window.location.replace('HomePage.html');
+
+  }
+  else {
+    alert('oops !!! bad credentials');
+  }
+}
