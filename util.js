d34dc15a069aca32655309ba394b853159b43d5b
lipunmishra <53727376+lipunmishra@users.noreply.github.com>
2019-08-06 09:52:00 +0530

    updated the logged text

--- util.js
+++ util.js
@@ -2,7 +2,7 @@ function validateUser() {
   var username=document.getElementById('username').value;
   var password=document.getElementById('password').value;
   if ("admin"==username && "admin"==password) {
-    alert('hey ur sucessfully looged in ,accept the alert');
+    alert('hey ur sucessfully logged in ,accept the alert');
     window.location.replace('HomePage.html');
 
   }
